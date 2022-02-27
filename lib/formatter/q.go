1c9b329c12c1c4a8a66cf718f36e254017cf3f4f
myOmikron <git@omikron.dev>
2022-02-27 12:16:27 +0100

    Added type assertion, as well as casting of timedatafields to float64datafields

--- lib/formatter/q.go
+++ lib/formatter/q.go
@@ -35,10 +35,25 @@ type out struct {
 }
 
 func FormatOutputQ(stdout string, st state.State, dataPoints ...interface{}) {
+	var dps = make([]interface{}, len(dataPoints))
+
+	for i := 0; i < len(dataPoints); i++ {
+		if v, ok := dataPoints[i].(*TimeDataPoint); ok {
+			dps = append(dps, Float64DataPoint{
+				Key:   v.Key,
+				Value: float64(v.Value) / float64(time.Millisecond),
+			})
+		} else if v, ok := dataPoints[i].(*Float64DataPoint); ok {
+			dps = append(dps, v)
+		} else if v, ok := dataPoints[i].(*IntDataPoint); ok {
+			dps = append(dps, v)
+		}
+	}
+
 	j, err := json.Marshal(out{
 		Stdout:   stdout,
 		State:    int(st),
-		Datasets: dataPoints,
+		Datasets: dps,
 	})
 	if err != nil {
 		fmt.Println(err.Error())
