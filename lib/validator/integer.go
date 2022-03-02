0db467e0cc821c54ef60d84b5e68a8ae37852f04
myOmikron <git@omikron.dev>
2022-03-02 04:16:39 +0100

    Added port range parser

--- lib/validator/integer.go
+++ lib/validator/integer.go
@@ -2,12 +2,20 @@ package validator
 
 import (
 	"errors"
+	"math"
 	"strconv"
 )
 
 func PositiveIntegerValidator(i string) (err error) {
 	if i, _ := strconv.Atoi(i); i <= 0 {
 		err = errors.New("value must be a positive int")
+	}
+	return
+}
+
+func PortValidator(i string) (err error) {
+	if i, _ := strconv.Atoi(i); i <= 0 || i > int(math.Pow(2, 16)-1) {
+		err = errors.New("value must be in the valid port range")
 	}
 	return
 }
